93b9eaa1504d6dc15de096409a52afaf63eb9cc0
locdv <dinhvanloc25@gmail.com>
2018-12-23 22:15:01 +0700

    add ut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
@@ -15,30 +16,25 @@ from .models import Question, Choice
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def results(request, question_id):
-    """
-    get question đối tượng bởi id
-    """
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        """
-        kiểm tra choice người dùng đã lựa chọn
-        nếu người dùng không lựa chọn sẽ hiển thị thông báo
-        hiển thị màn hình detail
-        """
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
@@ -48,7 +44,6 @@ def vote(request, question_id):
         })
     else:
         selected_choice.votes += 1
-        # lưu xuống database
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
