32c3ac5e4692d712bdc0b50342b5c8efebcb1ae6
locdv <dinhvanloc25@gmail.com>
2018-12-27 23:22:03 +0700

    Signed-off-by: locdv <dinhvanloc25@gmail.com>
    change gui of addmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
+"""
+ đăng kí thông tin được quản lý bởi admin
+"""
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # cung cấp 3 choice cho một question bởi mặc định
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    tạo section để hiển thị thông tin của question
+    date thông tin có thể collapse
+    """
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    """
+    class tham chieu
+    """
+    inlines = [ChoiceInline]
+    # thông tin phụ để hiển thị
+    list_display = ('question_text', 'pub_date')
+    # trường để filter
+    list_filter = ['pub_date']
+    # hiển thị box để search theo trường
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,6 @@ from django.utils import timezone
 # Create your models here.
 
 
-
 class Question(models.Model):
     """" Thông tin của question
     question_text: Nội dung của question
@@ -20,8 +19,9 @@ class Question(models.Model):
 
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published') # date published human readable.
-    
+    # date published human readable.
+    pub_date = models.DateTimeField('Date published')
+
     def __str__(self):
         return self.question_text
 
@@ -32,6 +32,7 @@ class Question(models.Model):
         now = timezone.now()
         return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
 class Choice(models.Model):
     """"
     question: Question có quan hệ với choice
@@ -41,7 +42,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
-
-    
