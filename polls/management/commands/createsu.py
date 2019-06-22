d41858cd15d4e80cde9606bccd79ee634a040d2a
locdv <dinhvanloc25@gmail.com>
2019-06-22 10:23:29 +0700

    update password

--- polls/management/commands/createsu.py
+++ polls/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@gmail.com", "admin")
+            User.objects.create_superuser("admin", "admin@gmail.com", "admin22222$")
